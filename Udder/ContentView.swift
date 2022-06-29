ff9741ab4f931a261b2a724e602cb499ba61bb54
Koki <koki.k.hayashi@gmail.com>
2022-06-29 16:05:21 -0700

    More !!!!1

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+
+        
         VStack {
             Label("Freshmilk on demand.", systemImage: "1.circle")
             List {
-                Text("Hello, world!")
+                Text("Hello, world!!!!!")
                     .padding()
 
                 
