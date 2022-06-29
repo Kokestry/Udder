954c372d977baaf0cb0d94876ccb052bd0e31dab
Koki <koki.k.hayashi@gmail.com>
2022-06-29 15:42:10 -0700

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Koki Hayashi on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
